088b19dcf9d59c4bd258442695f5c2a7a199e657
JoseBorbon <joserborbon@gmail.com>
2021-11-10 12:31:46 -0500

    add Heading component

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Heading extends Component {
+  render() {
+    return (
+      <>
+        <h1>React Jurassic Park</h1>
+        <h3>"Jurreactic Park"?</h3>
+        <h3>No. Just... no.</h3>
+      </>
+    );
+  }
+}
